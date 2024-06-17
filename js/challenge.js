2cb7af2efab4b7ac492f7c36d3380eca220b394d
Loser Facundo Dario <loserfacundodario@gmail.com>
2024-06-17 11:49:42 -0300

    adding all files

---
+++ js/challenge.js
@@ -0,0 +1,49 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+var RecaptchaState = {
+    site : '6LfxCbsSAAAAAKUJClt6W0t4-8VOCQlx0IFpprQs',
+    rtl : false,
+    challenge : '03AHJ_VutWHzAqnBBA4KL7rjoiYdmqzBXmdn2pxkQm-C_yYpMirIYglXMKWnv547PEeQLjaUAmOWOf2UMPCzBo7LU8Jb4p3NHOiaCtKYMatI0fRamYEbxV1nwM9OzFCVdver3MnYMV9-QneSMh8x0l86wf6cgFYdBFdmWR6_PX14yC12OJJvart_8',
+    is_incorrect : false,
+    programming_error : '',
+    error_message : '',
+    server : 'https://web.archive.org/web/20130313104500/http://www.google.com/recaptcha/api/',
+    lang : 'en-US',
+    timeout : 1800
+};
+
+document.write('<scr'+'ipt type="text/javascript" s'+'rc="' + RecaptchaState.server + 'js/recaptcha.js"></scr'+'ipt>');
+
+
+}
+/*
+     FILE ARCHIVED ON 10:45:00 Mar 13, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:04:56 Jun 15, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.002
+  exclusion.robots: 0.113
+  exclusion.robots.policy: 0.099
+  esindex: 0.012
+  cdx.remote: 29.687
+  LoadShardBlock: 152.205 (3)
+  PetaboxLoader3.resolve: 122.407 (3)
+  PetaboxLoader3.datanode: 77.51 (4)
+  load_resource: 89.099
+*/
